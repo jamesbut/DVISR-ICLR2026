cfc869ac1148078e93f249f02c2ef6f79ba13618
James Butterworth <jjbsports16@hotmail.co.uk>
2025-04-23 19:20:58 +0100

    Remove final match case in equation.py

--- src/algorithms/vicatsr/equation.py
+++ src/algorithms/vicatsr/equation.py
@@ -205,11 +205,10 @@ class Equation:
 
             # Increase or decrease the number of constants required
             # depending on the sample token type
-            match token['type']:
-                case 'bin_op':
-                    num_consts_required += 1
-                case 'const':
-                    num_consts_required -= 1
+            if token['type'] == 'bin_op':
+                num_consts_required += 1
+            elif token['type'] == 'const':
+                num_consts_required -= 1
 
     def convert_distr_to_opt_consts(self):
         for t in self._eq:
